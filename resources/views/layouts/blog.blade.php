10158a7745cfd670554c950380e6a123eac029a9
= <=>
2017-08-13 21:43:20 -0800

    Set up blogging layout

---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,18 @@
+
+
+@extends('layouts.layout')
+
+@section ('content')
+
+<div id='postContent'>
+    
+    @foreach($blogpost as $post)
+     @include('components.blog.blogpost')
+    <hr>
+    
+ @endforeach
+  
+</div>
+
+
+@endsection
