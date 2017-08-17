6a9a0e6c50a972da29a89d5374cce37ad1d52652
= <=>
2017-08-16 22:42:15 -0800

    readded Post

---
+++ app/Http/Controllers/PostsControllers.php
@@ -0,0 +1,106 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
+use Carbon\Carbon;
+class PostsController extends Controller
+{
+    
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        
+        $posts = Post::latest()
+                ->filter(request(['Month', 'Year']))
+                ->get();
+        return view('layouts.blog.blog', ['posts' => $posts]);
+       
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      
+        return view('layouts.blog.blogmake', []);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $this->validate(request(), [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        $blogpost = new Post();
+        $blogpost->postName = request('title');
+        $blogpost->postContent = request('body');
+        $blogpost->user_id = auth()->id();
+        $blogpost->save();
+        return redirect('/');
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show( $id)
+    { 
+      $posts = Post::find($id);
+       
+        //dd( $posts);
+        return view('layouts.blog.blog', ['posts' => [$posts]]);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::find($id);
+        $post->postName = $request->get('title');
+        $post->postContent = $request->get('body');
+        $post->save();
+        return redirect('/');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
